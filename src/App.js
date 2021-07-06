d0eb717fc2df23acf80b3f78920853753d7d0285
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-07-06 13:29:28 +0200

    Looping through mock data in parent component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 import "./styles.css";
+import { mockPokemonsData } from "../src/mock/pokeData.js";
 import { PokeList } from "./components/PokeCards.jsx";
 
 export default function App() {
+  const pokemons = mockPokemonsData;
+  console.log("data", pokemons);
+  const { name, stripes, video } = pokemons;
+
   return (
     <div className="App">
       <h1>2o. Challenge: Loops</h1>
-      <PokeList />
+      {pokemons.map((pokemon) => (
+        <div className="container">
+          <div className="child-container">
+            <PokeList
+              key={pokemon.id}
+              name={name}
+              stripes={stripes}
+              video={video}
+            />
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/PokeCards.jsx
+++ src/components/PokeCards.jsx
@@ -1,8 +1,20 @@
 import React from "react";
-import mockPokemonsData from "../mock/pokeData.js";
+// import { mockPokemonsData } from "../mock/pokeData.js";
 
-export const PokeList = (data) => {
-  console.log("Pokemon list", mockPokemonsData);
+export const PokeList = (pokemons) => {
+  console.log("pokemon", pokemons);
+  const { name, sprites, video } = pokemons;
 
-  return <div>New poke card</div>;
+  return (
+    <div>
+      <h1>Poke cards</h1>
+      <ul>
+        <li>{name}</li>
+        <li>
+          <img className="card-img" src={sprites} alt="" />
+        </li>
+        <li>{video}</li>
+      </ul>
+    </div>
+  );
 };
