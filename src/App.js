a1a219fb38838f36c05bfb720b69b956e272c82d
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-07-07 10:51:56 +0200

    Displaying data in the screen

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import { PokeList } from "./components/PokeCards.jsx";
 export default function App() {
   const pokemons = mockPokemonsData;
   console.log("data", pokemons);
-  const { name, stripes, video } = pokemons;
+  // const { name, stripes, video } = pokemons;
 
   return (
     <div className="App">
-      <h1>2o. Challenge: Loops</h1>
-      {pokemons.map((pokemon) => (
+      <h1>Loops</h1>
+      {pokemons.map((pokemon, index) => (
         <div className="container">
-          <div className="child-container">
+          <div className="child-container" key={pokemon.id}>
             <PokeList
-              key={pokemon.id}
-              name={name}
-              stripes={stripes}
-              video={video}
+              name={pokemon.name}
+              stripesFront={pokemon.sprites.front_default}
+              stripesFront2={pokemon.sprites.front_shiny}
+              stripesBack={pokemon.sprites.back_default}
+              stripesBack2={pokemon.sprites.back_shiny}
+              videoLink={pokemon.videoLink}
             />
           </div>
         </div>
--- src/components/PokeCards.jsx
+++ src/components/PokeCards.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-// import { mockPokemonsData } from "../mock/pokeData.js";
+import { mockPokemonsData } from "../mock/pokeData.js";
 
 export const PokeList = (pokemons) => {
   console.log("pokemon", pokemons);
-  const { name, sprites, video } = pokemons;
+  const { name, stripesFront, stripesBack, videoLink } = pokemons;
 
   return (
     <div>
       <h1>Poke cards</h1>
       <ul>
         <li>{name}</li>
-        <li>
-          <img className="card-img" src={sprites} alt="" />
-        </li>
-        <li>{video}</li>
+        <a href={videoLink}>
+          <img className="card-img" src={stripesFront} alt="" />
+          <img className="card-img" src={stripesBack} alt="" />
+        </a>
       </ul>
     </div>
   );
